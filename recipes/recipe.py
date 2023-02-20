6d80985c00cdf3694d810a300349e80b1fe0dc78
mss12138 <17839221453@163.com>
2023-02-20 21:10:18 +0800

    Update recipe.py
    加入了关键时期的提醒

--- recipes/recipe.py
+++ recipes/recipe.py
@@ -6,14 +6,25 @@ def Recipe1():
     title = "--------【今日菜谱】--------\n"
     today = datetime.datetime.now()
     Today = "        今天是" + str(today.year) + "年" + str(today.month) + "月" + str(today.day) + "日\n"
-    
+    OtherHappy=[]
+    map = ["2023-03-25,打第二针疫苗", "2023-05-20,生日"] 
+    for s in map:
+        that_1 = datetime.datetime.strptime(s.split(',')[0], '%Y-%m-%d')
+        dayname = s.split(',')[1]
+        if int((that_1 - today).days + 1) > 0:
+            a = "距离" + dayname + "还有" + str((that_1 - today).days + 1) + "天\n\n"
+            OtherHappy.append(a)
+    expression=['(￣▽￣)~*','(；′⌒`)',' (*T_T*) ','ヾ(๑╹◡╹)ﾉ"','(ﾉﾟ▽ﾟ)ﾉ','(^_−)☆','❥(^_-)','(－ｏ⌒)✿ ',
+            '☆￣(＞。☆)','(▼ヘ▼#)','ヽ(●-`Д´-)ノ','(╯°Д°)╯','(ー`´ー)','(｡・`ω´･)','d(･｀ω´･d*)','(•́へ•́╬)'
+        ]
     that='2022-04-17'
     start_day = datetime.datetime.strptime(that, '%Y-%m-%d')
     num=str((today-start_day).days)
-    with open(f'./recipes/texts/{num}.txt', 'r',  encoding='utf-8') as f: #在GitHub与本地写文件地址的格式不同
+    # with open(f'./texts/{num}.txt', 'r',  encoding='utf-8') as f: #在本地调试
+    with open(f'./recipes/texts/{num}.txt', 'r',  encoding='utf-8') as f: #在GitHub上运行
         text=f.read()
     copyright="\nRemixed by mss"
-    content=title + Today + text + copyright
+    content=title + Today + random.choice(expression).join(OtherHappy)+ text + copyright
     return content
     
 def send1(content):
@@ -33,16 +44,24 @@ def send1(content):
 def Recipe2():
     title = "----------------【今日菜谱】---------------"
     today = datetime.datetime.now()
-    Today = "(^_^)/(T_T)今天是" + str(today.year) + "年" + str(today.month) + "月" + str(today.day) + "日(*^o^)人(^o^*)"
-    
+    Today = "(^_^)/(T_T)今天是" + str(today.year) + "年" + str(today.month) + "月" + str(today.day) + "日(*^o^)人(^o^*)\n\n"
+    OtherHappy=[Today]
+    map = ["2023-03-25,打第二针疫苗", "2023-05-20,生日"] 
+    for s in map:
+        that_1 = datetime.datetime.strptime(s.split(',')[0], '%Y-%m-%d')
+        dayname = s.split(',')[1]
+        if int((that_1 - today).days + 1) > 0:
+            a = "距离" + dayname + "还有" + str((that_1 - today).days + 1) + "天\n"
+            OtherHappy.append(a)
     that='2022-04-17'
     start_day = datetime.datetime.strptime(that, '%Y-%m-%d')
     num=str((today-start_day).days)
-    with open(f'./recipes/texts/{num}.txt', 'r',  encoding='utf-8') as f: #在GitHub与本地写文件地址的格式不同
+    # with open(f'./texts/{num}.txt', 'r',  encoding='utf-8') as f: #在本地调试
+    with open(f'./recipes/texts/{num}.txt', 'r',  encoding='utf-8') as f: #在GitHub上运行
         l=f.readlines()
     text='<br>'.join(l)
     copyright='Remixed by mss'
-    content=title + '<br>' + Today + '<br>'+ text + '<br>' +copyright
+    content=title + '<br>'+ '(^_−)☆'.join(OtherHappy) + '<br>'+ text + '<br>' +copyright
     return content
 
 def send2(content):
@@ -53,13 +72,15 @@ def send2(content):
     url = 'http://pushplus.plus/send?token='+token+'&title='+title+'&content='+content+'&topic='+topic
     return requests.get(url)
 
-
-
-
+    # #下面是测试时候单独发给自己的信息，实际操作时候请注释
+    # token = 'b04d9467b5bd4316bb9105f72d6f8b6f' #在pushplus网站中可以找到
+    # title= '今天的菜谱来啦！' #改成你要的标题内容
+    # url = 'http://pushplus.plus/send?token='+token+'&title='+title+'&content='+content
+    # return requests.get(url)
 
 def main():
     text1=Recipe1()
-    send1(text1)
+    # send1(text1)
 
     text2=Recipe2()
     send2(text2)
